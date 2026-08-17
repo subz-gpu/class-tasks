4d495100dcd5c5b7e82b545a68f83340e3d1a7fb
subz <subhamdewan07@gmail.com>
2026-08-17 15:13:43 +0530

    second commit

---
+++ prg1.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const hostname = 'localhost';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, Node.js World!\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log('Press Ctrl+C to stop the server');
+});
